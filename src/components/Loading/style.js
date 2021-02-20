b9e7487b0d6ba90dfee8c466787c130ffe663746
BinhTT1312 <binhtt.13122000@gmail.com>
2021-02-21 04:38:01 +0700

    refactor loading

---
+++ src/components/Loading/style.js
@@ -0,0 +1,13 @@
+import { makeStyles } from "@material-ui/core";
+
+export const useStyles = makeStyles({
+    loadingImage: {
+        display: 'block',
+        margin: '0 auto',
+        marginTop: 'auto',
+        marginBottom: 'auto'
+    },
+    loadingBackground: {
+        minHeight: '70vh'
+    }
+})
