2c2982889a8e1715adfa4948d1bca1ac56168b44
BinhTT1312 <binhtt.13122000@gmail.com>
2021-02-21 14:44:24 +0700

    fix app bar

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -2,16 +2,14 @@ import { makeStyles } from "@material-ui/core";
 
 export const useStyles = makeStyles((theme) => ({
   appBar: {
-    [theme.breakpoints.up('md')]: {
       zIndex: theme.zIndex.drawer + 1
       // width: `calc(100% - ${drawerWidth}px)`,
       // marginLeft: drawerWidth,
-    },
   },
   menuButton: {
     marginRight: theme.spacing(2),
     [theme.breakpoints.up('md')]: {
-      display: 'none',
+      display: 'none !important',
     },
   },
   logo: {
